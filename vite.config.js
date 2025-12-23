c09682782944ab9cfa5842e2b6f06d9844a05eb9
Kelvin Choo <59645923+kelza23@users.noreply.github.com>
2025-12-23 22:00:25 +1100

    fixing path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kelza23/", // CRITICAL: This must match your repo name exactly
+  base: "/", // CRITICAL: This must match your repo name exactly
 })
